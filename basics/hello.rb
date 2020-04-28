1b5245aa88ea608ea540ab979a4090779474f194
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-04-28 13:38:18 +0300

    add koans

--- basics/hello.rb
+++ basics/hello.rb
@@ -2,11 +2,15 @@
 
 print "Hello Ruby!\n"    # prints the welcome message
 
+# COMMENTING
+
 =begin
 This is a comment line
 it explains that the next line of code displays 
 a welcome message
 =end
+
+# VARIABLES
 
 # Unlike other programming languages, Ruby actually allows the value assigned to a constant to be changed
 # after it has been created. (With warning)
@@ -24,4 +28,20 @@ print(y.class)
 # integer to float
 y = 20
 y.to_f
+
+# VARIABLE SCOPE
+
+# defined? will return the scope of the variable referenced, or nil if the variable is not defined in the
+# current context:
+x = 10
+defined? x
+# => "local-variable"
+
+# Variable scope
+_local = "this is a local variable"
+$global = "this is a global variable"
+
+
+
+
 
